00222f2bc5c241f8952c33a177e017ff25d85938
JLScheewel <73995194+JLScheewel@users.noreply.github.com>
2021-11-15 18:55:56 +0100

    Assignments upload

--- Assignment1PrimeNumbers.py
+++ Assignment1PrimeNumbers.py
@@ -1,11 +1,11 @@
-N = int(input("Please select here the number of primes you want to print:"))
-a = 2
+N = int(input("Please select here the number N of primes you want to print:"))
+a = 2 # initializing the starting value (as 0 and 1 is no prime number by definition)
 
-while N != 0:
-    for i in range (2,a):
-        if a%i == 0:
+while N != 0: # repeat 'for loop' as long as the remaining numbers of primes to print is not 0:
+    for i in range (2,a): # 'for loop' sequence, start = 2, stop = a
+        if a%i == 0: # if the number is divisible by i, then a is not a prime number
             break
-    else:
-        print(a,end= " ")
-        N -= 1
-    a += 1
+    else: #otherwise, n is prime number
+        print(a,end = ", ") # print a and create a blank space
+        N -= 1 # update the remaining amount of prime numbers to print by -1
+    a += 1 # update the starting value by +1
--- Assignment2TextRead.py
+++ Assignment2TextRead.py
@@ -1,6 +1,6 @@
-#text: https://cooking.nytimes.com/recipes/1022727-classic-apple-pie
+#text: https://www.gutenberg.org/files/10/10-0.txt
 
-file = 'ApplePie.txt'
+file = 'Bible.txt'
 with open(file, 'r') as file_in:
     lines = file_in.read().replace(",", " ").replace(".", " ").replace("(", "").replace(")", " ").lower().split()
 from collections import Counter
